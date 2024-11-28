c4b78c5a12ef3820a28e347b3d801518cffaccb9
DmitriiDudarow <dudarow.d@yandex.ru>
2024-11-29 01:03:47 +0500

    Dudarow Task 5

---
+++ task05/src/com/example/task05/MailService.java
@@ -0,0 +1,35 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<Message<T>> {
+    HashMap<String, List<T>> mailBox = new HashMap<String, List<T>>() {
+
+        @Override
+        public List<T> get(Object key) {
+            return super.get(key) == null ? new ArrayList<>() : super.get(key);
+        }
+    };
+
+    public MailService() {
+
+    }
+
+    public HashMap<String, List<T>> getMailBox() {
+        return mailBox;
+    }
+
+    @Override
+    public void accept(Message<T> message) {
+        if (!mailBox.containsKey(message.getTo())) {
+            mailBox.put(message.getTo(), new ArrayList<>());
+            mailBox.get(message.getTo()).add(message.getContent());
+        } else {
+            mailBox.get(message.getTo()).add(message.getContent());
+        }
+    }
+
+}
---
+++ task05/src/com/example/task05/Message.java
@@ -0,0 +1,9 @@
+package com.example.task05;
+
+public interface Message<T> {
+    String getFrom();
+
+    String getTo();
+
+    T getContent();
+}
